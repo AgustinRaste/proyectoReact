9bcad726c9d4896fc507d5f260bacd0c7b6aed24
agustinraste <rastellini.a@gmail.com>
2021-09-28 13:40:40 -0300

    Se creo el componente NavBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h2>LOGO</h2>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">TIENDA</a></li>
+                <li><a href="">NOSOTROS</a></li>
+                <li><a href="">CONTACTO</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
